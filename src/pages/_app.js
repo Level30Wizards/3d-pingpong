430f174f40551eedb3cf21e9838eb64054f67e77
Mees Rutten <meesrutten@192.168.0.170>
2021-02-25 11:02:40 +0100

    add if statement

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -42,8 +42,14 @@ function MyApp({ Component, pageProps }) {
    * why-did-you-render README
    * https://github.com/welldone-software/why-did-you-render
    */
-  const whyDidYouRender = require('@welldone-software/why-did-you-render')
-  whyDidYouRender(React)
+  if (process.env.NODE_ENV === 'development') {
+    if (typeof window !== 'undefined') {
+      const whyDidYouRender = require('@welldone-software/why-did-you-render')
+      whyDidYouRender(React, {
+        trackAllPureComponents: true,
+      })
+    }
+  }
 
   /**
    * To debug a component:
